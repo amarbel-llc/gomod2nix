24c7a2b7d59cfeabc031bcb0d1218273b6ae065b
sfriedenberg-etsy <sfriedenberg@etsy.com>
2026-06-24 20:38:24 -0400

    bump nixpkgs-master: d233902 → 567a49d

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Convert go.mod/go.sum to Nix packages";
 
-  inputs.nixpkgs-master.url = "github:NixOS/nixpkgs/d233902339c02a9c334e7e593de68855ad26c4cb";
+  inputs.nixpkgs-master.url = "github:NixOS/nixpkgs/567a49d1913ce81ac6e9582e3553dd90a955875f";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs =
